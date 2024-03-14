3aff1f85e1ada79d7b603a356f128b7c27f66f37
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2024-03-14 18:31:57 +0200

    Create test templates for four of the test models available

---
+++ tests/test_markovian_marking.py
@@ -0,0 +1,74 @@
+import pytest
+
+from process_running_state.markovian_marking import MarkovianMarking
+from test_bpmn_model_fixtures import _bpmn_model_with_loop_inside_AND, _bpmn_model_with_AND_and_nested_XOR, \
+    _bpmn_model_with_XOR_within_AND, _bpmn_model_with_AND_and_XOR
+
+
+@pytest.mark.skip(reason="Test not yest finished")
+def test_build_simple():
+    bpmn_model = _bpmn_model_with_AND_and_XOR()
+    reachability_graph = bpmn_model.get_reachability_graph()
+    markovian_marking = MarkovianMarking(reachability_graph, n_gram_size_limit=3)
+    markovian_marking.build()
+    # Check there is one entry per activity label (plus the empty trace)
+    size_1_n_grams = [n_gram for n_gram in markovian_marking.markings if len(n_gram) == 1]
+    assert len(size_1_n_grams) == len(reachability_graph.activity_to_edges) + 1
+    # Check the maximum size of the explored n-grams is under the limit
+    n_gram_sizes = [len(n_gram) for n_gram in markovian_marking.markings]
+    assert max(n_gram_sizes) <= 3
+    # Check specific markings
+    assert markovian_marking.get_marking_state(["A"]) == {{5, 6}}
+    assert markovian_marking.get_marking_state(["B"]) == {{9, 6}, {12}}
+    assert markovian_marking.get_marking_state(["C"]) == {{5, 10}, {12}}
+    assert markovian_marking.get_marking_state(["D"]) == {{21}}
+    assert markovian_marking.get_marking_state(["E"]) == {{21}}
+    assert markovian_marking.get_marking_state(["F"]) == {{25}}
+
+
+@pytest.mark.skip(reason="Test not yest finished")
+def test_build_XOR_within_AND():
+    bpmn_model = _bpmn_model_with_XOR_within_AND()
+    reachability_graph = bpmn_model.get_reachability_graph()
+    markovian_marking = MarkovianMarking(reachability_graph, n_gram_size_limit=3)
+    markovian_marking.build()
+    # Check there is one entry per activity label
+    size_1_n_grams = [n_gram for n_gram in markovian_marking.markings if len(n_gram) == 1]
+    assert len(size_1_n_grams) == len(reachability_graph.activity_to_edges)
+    # Check the maximum size of the explored n-grams is under the limit
+    n_gram_sizes = [len(n_gram) for n_gram in markovian_marking.markings]
+    assert max(n_gram_sizes) <= 3
+    # Check specific markings
+    # TODO
+
+
+@pytest.mark.skip(reason="Test not yest finished")
+def test_build_nested_XOR():
+    bpmn_model = _bpmn_model_with_AND_and_nested_XOR()
+    reachability_graph = bpmn_model.get_reachability_graph()
+    markovian_marking = MarkovianMarking(reachability_graph, n_gram_size_limit=3)
+    markovian_marking.build()
+    # Check there is one entry per activity label
+    size_1_n_grams = [n_gram for n_gram in markovian_marking.markings if len(n_gram) == 1]
+    assert len(size_1_n_grams) == len(reachability_graph.activity_to_edges)
+    # Check the maximum size of the explored n-grams is under the limit
+    n_gram_sizes = [len(n_gram) for n_gram in markovian_marking.markings]
+    assert max(n_gram_sizes) <= 3
+    # Check specific markings
+    # TODO
+
+
+@pytest.mark.skip(reason="Test not yest finished")
+def test_build_loop_model():
+    bpmn_model = _bpmn_model_with_loop_inside_AND()
+    reachability_graph = bpmn_model.get_reachability_graph()
+    markovian_marking = MarkovianMarking(reachability_graph, n_gram_size_limit=3)
+    markovian_marking.build()
+    # Check there is one entry per activity label
+    size_1_n_grams = [n_gram for n_gram in markovian_marking.markings if len(n_gram) == 1]
+    assert len(size_1_n_grams) == len(reachability_graph.activity_to_edges)
+    # Check the maximum size of the explored n-grams is under the limit
+    n_gram_sizes = [len(n_gram) for n_gram in markovian_marking.markings]
+    assert max(n_gram_sizes) <= 3
+    # Check specific markings
+    # TODO
