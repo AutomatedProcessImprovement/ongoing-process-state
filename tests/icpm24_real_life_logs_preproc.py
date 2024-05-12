8965fecbb7ce983fcdd819647cb4edd32ae82b98
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2024-05-12 14:07:37 +0300

    Retain only needed columns for real-life logs and print stats for each log

--- tests/icpm24_real_life_logs_preproc.py
+++ tests/icpm24_real_life_logs_preproc.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import List
 
 import pandas as pd
-from pix_framework.io.event_log import DEFAULT_CSV_IDS, EventLogIDs
+from pix_framework.io.event_log import DEFAULT_CSV_IDS, EventLogIDs, DEFAULT_XES_IDS
 from pm4py.objects.log.exporter.csv.versions.pandas_csv_exp import get_dataframe_from_log
 from pm4py.objects.log.exporter.xes.factory import export_log
 from pm4py.objects.log.importer.csv.versions.pandas_df_imp import convert_dataframe_to_event_log
@@ -19,29 +19,32 @@ def read_and_process_datasets(datasets: List[str]):
         # Read XES event log
         original_xes_log = xes_import_factory.apply(xes_file_path)
         # Convert to DataFrame and filter if necessary
-        csv_log = get_dataframe_from_log(original_xes_log).sort_values('case:concept:name')
+        csv_log = get_dataframe_from_log(original_xes_log).sort_values(DEFAULT_XES_IDS.case)
         if "lifecycle:transition" in csv_log.columns:
             csv_log = csv_log[csv_log["lifecycle:transition"].isin([
                 "complete", "COMPLETE",
                 "Closed", "Cancelled",
                 "ate_abort", "withdraw"
             ])]
+        # Keep only needed columns
+        csv_log = csv_log[[DEFAULT_XES_IDS.case, DEFAULT_XES_IDS.activity, DEFAULT_XES_IDS.end_time]]
+        print_stats(csv_log, DEFAULT_XES_IDS)
         # Export to XES
         xes_log = convert_dataframe_to_event_log(csv_log)
         export_log(xes_log, output_xes_path, parameters={"compress": True})
         # Export to CSV
         csv_log.rename(columns={
-            'case:concept:name': DEFAULT_CSV_IDS.case,
-            'concept:name': DEFAULT_CSV_IDS.activity,
-            'time:timestamp': DEFAULT_CSV_IDS.end_time,
-            'org:resource': DEFAULT_CSV_IDS.resource,
+            DEFAULT_XES_IDS.case: DEFAULT_CSV_IDS.case,
+            DEFAULT_XES_IDS.activity: DEFAULT_CSV_IDS.activity,
+            DEFAULT_XES_IDS.end_time: DEFAULT_CSV_IDS.end_time,
         }, inplace=True)
         csv_log.to_csv(output_csv_path, index=False)
 
 
 def read_and_process_bpic_2014():
-    # Define paths
     dataset = "BPIC_2014_Activity_log_for_incidents"
+    print(f"\n--- Processing {dataset} ---\n")
+    # Define paths
     csv_file_path = f"../inputs/real-life/original/{dataset}.csv"
     output_xes_path = f"../outputs/{dataset}.xes"
     output_csv_path = Path(f"../outputs/{dataset}.csv.gz")
@@ -57,27 +60,44 @@ def read_and_process_bpic_2014():
     csv_log[log_ids.resource] = csv_log[log_ids.resource].apply(str)
     csv_log[log_ids.end_time] = pd.to_datetime(csv_log[log_ids.end_time], utc=True, format="%d-%m-%Y %H:%M:%S")
     csv_log = csv_log.sort_values(log_ids.end_time)
+    # Keep only needed columns
+    csv_log = csv_log[[log_ids.case, log_ids.activity, log_ids.end_time]]
+    print_stats(csv_log, log_ids)
     # Export to XES
     csv_log.rename(columns={
-        log_ids.case: 'case:concept:name',
-        log_ids.activity: 'concept:name',
-        log_ids.end_time: 'time:timestamp',
-        log_ids.resource: 'org:resource',
+        log_ids.case: DEFAULT_XES_IDS.case,
+        log_ids.activity: DEFAULT_XES_IDS.activity,
+        log_ids.end_time: DEFAULT_XES_IDS.end_time,
     }, inplace=True)
     xes_log = convert_dataframe_to_event_log(csv_log)
     export_log(xes_log, output_xes_path, parameters={"compress": True})
     # Export to CSV
     csv_log.rename(columns={
-        'case:concept:name': DEFAULT_CSV_IDS.case,
-        'concept:name': DEFAULT_CSV_IDS.activity,
-        'time:timestamp': DEFAULT_CSV_IDS.end_time,
-        'org:resource': DEFAULT_CSV_IDS.resource,
+        DEFAULT_XES_IDS.case: DEFAULT_CSV_IDS.case,
+        DEFAULT_XES_IDS.activity: DEFAULT_CSV_IDS.activity,
+        DEFAULT_XES_IDS.end_time: DEFAULT_CSV_IDS.end_time,
     }, inplace=True)
     csv_log.to_csv(output_csv_path, index=False)
 
 
+def print_stats(event_log: pd.DataFrame, log_ids: EventLogIDs):
+    number_cases = len(event_log[log_ids.case].unique())
+    number_activities = len(event_log[log_ids.activity].unique())
+    number_activity_instances = len(event_log)
+    minimum_lenght = min([len(events) for case_id, events in event_log.groupby(log_ids.case)])
+    average_lenght = round(number_activity_instances / number_cases)
+    maximum_lenght = max([len(events) for case_id, events in event_log.groupby(log_ids.case)])
+    print(f"\tCases: {number_cases}\n"
+          f"\tActivities: {number_activities}\n"
+          f"\tActivity Instances: {number_activity_instances}\n"
+          f"\tMin length: {minimum_lenght}\n"
+          f"\tAvg length: {average_lenght}\n"
+          f"\tMax length: {maximum_lenght}\n\n")
+
+
 if __name__ == '__main__':
     read_and_process_datasets([
+        "BPIC_2011_hospital_log",
         "BPIC_2012",
         "BPIC_2013_closed_problems",  # These logs get reduced to only 1 event per case, also the activity
         "BPIC_2013_incidents",  # names go from ["Queued", "Accepted", "Completed", "Unmatched"] to just
@@ -97,6 +117,5 @@ if __name__ == '__main__':
         "BPIC_2020_TravelPermitData",
         "Road_Traffic_Fine_Management_Process",
         "Sepsis_Cases",
-        "BPIC_2011_hospital_log",
     ])
     read_and_process_bpic_2014()
