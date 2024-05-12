d255ed6b08af7b3d91fdd26a77bac140ed50ad02
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2024-05-12 13:49:00 +0300

    Add preprocess of BPIC 2014 to the script

--- tests/icpm24_real_life_logs_preproc.py
+++ tests/icpm24_real_life_logs_preproc.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 from typing import List
 
-from pix_framework.io.event_log import DEFAULT_CSV_IDS
+import pandas as pd
+from pix_framework.io.event_log import DEFAULT_CSV_IDS, EventLogIDs
 from pm4py.objects.log.exporter.csv.versions.pandas_csv_exp import get_dataframe_from_log
 from pm4py.objects.log.exporter.xes.factory import export_log
 from pm4py.objects.log.importer.csv.versions.pandas_df_imp import convert_dataframe_to_event_log
@@ -38,11 +39,48 @@ def read_and_process_datasets(datasets: List[str]):
         csv_log.to_csv(output_csv_path, index=False)
 
 
+def read_and_process_bpic_2014():
+    # Define paths
+    dataset = "BPIC_2014_Activity_log_for_incidents"
+    csv_file_path = f"../inputs/real-life/original/{dataset}.csv"
+    output_xes_path = f"../outputs/{dataset}.xes"
+    output_csv_path = Path(f"../outputs/{dataset}.csv.gz")
+    # Read CSV event log
+    log_ids = EventLogIDs(
+        case="Incident ID", activity="IncidentActivity_Type",
+        resource="Assignment Group", end_time="DateStamp"
+    )
+    # Read log
+    csv_log = pd.read_csv(csv_file_path, sep=";")
+    csv_log = csv_log.astype({log_ids.case: object})
+    csv_log[log_ids.resource].fillna("NOT_SET", inplace=True)
+    csv_log[log_ids.resource] = csv_log[log_ids.resource].apply(str)
+    csv_log[log_ids.end_time] = pd.to_datetime(csv_log[log_ids.end_time], utc=True, format="%d-%m-%Y %H:%M:%S")
+    csv_log = csv_log.sort_values(log_ids.end_time)
+    # Export to XES
+    csv_log.rename(columns={
+        log_ids.case: 'case:concept:name',
+        log_ids.activity: 'concept:name',
+        log_ids.end_time: 'time:timestamp',
+        log_ids.resource: 'org:resource',
+    }, inplace=True)
+    xes_log = convert_dataframe_to_event_log(csv_log)
+    export_log(xes_log, output_xes_path, parameters={"compress": True})
+    # Export to CSV
+    csv_log.rename(columns={
+        'case:concept:name': DEFAULT_CSV_IDS.case,
+        'concept:name': DEFAULT_CSV_IDS.activity,
+        'time:timestamp': DEFAULT_CSV_IDS.end_time,
+        'org:resource': DEFAULT_CSV_IDS.resource,
+    }, inplace=True)
+    csv_log.to_csv(output_csv_path, index=False)
+
+
 if __name__ == '__main__':
     read_and_process_datasets([
         "BPIC_2012",
-        "BPIC_2013_closed_problems",  # These logs get reduced to only 1 event per case, also the activity 
-        "BPIC_2013_incidents",  # names go from ["Queued", "Accepted", "Completed", "Unmatched"] to just 
+        "BPIC_2013_closed_problems",  # These logs get reduced to only 1 event per case, also the activity
+        "BPIC_2013_incidents",  # names go from ["Queued", "Accepted", "Completed", "Unmatched"] to just
         "BPIC_2013_open_problems",  # ["Completed"], etc. bit shitty process
         "BPIC_2015_1",
         "BPIC_2015_2",
@@ -61,3 +99,4 @@ if __name__ == '__main__':
         "Sepsis_Cases",
         "BPIC_2011_hospital_log",
     ])
+    read_and_process_bpic_2014()
