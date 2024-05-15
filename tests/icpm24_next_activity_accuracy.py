454b749b2d848f64d9937057d4417239d95c4a5b
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2024-05-16 00:25:22 +0300

    Refactor script to evaluate in real-life logs

--- tests/icpm24_next_activity_accuracy.py
+++ tests/icpm24_next_activity_accuracy.py
@@ -8,23 +8,24 @@ from pix_framework.io.event_log import read_csv_log, DEFAULT_CSV_IDS
 from process_running_state.reachability_graph import ReachabilityGraph
 
 log_ids = DEFAULT_CSV_IDS
+output_file_path = Path(f"../outputs/synthetic_accuracy.csv")
 
 
-def next_activity_accuracy(datasets: List[str]):
+def next_activity_accuracy(datasets: List[str], discovery_extension: str = ""):
     # For each dataset
     for dataset in datasets:
         print(f"\n--- Dataset: {dataset} ---\n")
         # Instantiate paths
-        remaining_cases_path = Path(f"../inputs/{dataset}_remaining.csv.gz")
-        computed_states_path = Path(f"../inputs/{dataset}_ongoing_states.csv")
-        reachability_graph_path = Path(f"../inputs/{dataset}_reachability_graph.tgf")
+        remaining_cases_path = Path(f"../inputs/real-life/split/{dataset}_remaining.csv.gz")
+        computed_states_path = Path(f"../results/real-life/{dataset}{discovery_extension}_ongoing_states.csv")
+        reachability_graph_path = Path(f"../results/real-life/{dataset}{discovery_extension}_reachability_graph.tgf")
         # Read preprocessed event log(s)
         remaining_cases = read_csv_log(remaining_cases_path, log_ids, sort=True)
         computed_states = pd.read_csv(computed_states_path)
         with open(reachability_graph_path, "r") as reachability_graph_file:
             reachability_graph = ReachabilityGraph.from_tgf_format(reachability_graph_file.read())
         # Go over each case ID
-        iasr, ias, occ, mark3, mark5, mark7, mark9 = [], [], [], [], [], [], []
+        iasr, ias, occ, mark3, mark5, mark10 = [], [], [], [], [], []
         for case_id, data in computed_states.groupby("case_id"):
             # Retrieve remaining activities of this case
             remaining_case = remaining_cases[remaining_cases[log_ids.case] == case_id]
@@ -34,16 +35,15 @@ def next_activity_accuracy(datasets: List[str]):
             occ += [evaluate_state_approximation(data, "OCC", reachability_graph, remaining_case)]
             mark3 += [evaluate_state_approximation(data, "marking-3", reachability_graph, remaining_case)]
             mark5 += [evaluate_state_approximation(data, "marking-5", reachability_graph, remaining_case)]
-            mark7 += [evaluate_state_approximation(data, "marking-7", reachability_graph, remaining_case)]
-            mark9 += [evaluate_state_approximation(data, "marking-9", reachability_graph, remaining_case)]
-        # Print stats
-        print(f"IASR: {sum(iasr) / len(iasr)}")
-        print(f"IAS: {sum(ias) / len(ias)}")
-        print(f"OCC: {sum(occ) / len(occ)}")
-        print(f"mark-3: {sum(mark3) / len(mark3)}")
-        print(f"mark-5: {sum(mark5) / len(mark5)}")
-        print(f"mark-7: {sum(mark7) / len(mark7)}")
-        print(f"mark-9: {sum(mark9) / len(mark9)}\n")
+            mark10 += [evaluate_state_approximation(data, "marking-10", reachability_graph, remaining_case)]
+        # Write stats to file
+        with open(output_file_path, "a") as output_file:
+            output_file.write(f"{dataset},{discovery_extension},IASR,{sum(iasr)},{sum(iasr) / len(iasr)}\n")
+            output_file.write(f"{dataset},{discovery_extension},IAS,{sum(ias)},{sum(ias) / len(ias)}\n")
+            output_file.write(f"{dataset},{discovery_extension},OCC,{sum(occ)},{sum(occ) / len(occ)}\n")
+            output_file.write(f"{dataset},{discovery_extension},mark-3,{sum(mark3)},{sum(mark3) / len(mark3)}\n")
+            output_file.write(f"{dataset},{discovery_extension},mark-5,{sum(mark5)},{sum(mark5) / len(mark5)}\n")
+            output_file.write(f"{dataset},{discovery_extension},mark-10,{sum(mark10)},{sum(mark10) / len(mark10)}\n")
 
 
 def evaluate_state_approximation(
@@ -67,5 +67,29 @@ def evaluate_state_approximation(
     return is_enabled
 
 
+def _create_output_file():
+    with open(output_file_path, "w") as output_file:
+        output_file.write("dataset,discovery_algorithm,technique,abs_positives,rel_positives\n")
+
+
 if __name__ == '__main__':
-    next_activity_accuracy(["sepsis_cases"])
+    # Create output metrics path
+    _create_output_file()
+    # Process results
+    next_activity_accuracy([
+        "Sepsis_Cases",
+        "BPIC_2012",
+        "BPIC_2015_1",
+        "BPIC_2015_2",
+        "BPIC_2015_3",
+        "BPIC_2015_4",
+        "BPIC_2015_5",
+        "BPIC_2017",
+        "BPIC_2018",
+        "BPIC_2019",
+        "BPIC_2020_DomesticDeclarations",
+        "BPIC_2020_InternationalDeclarations",
+        "BPIC_2020_PrepaidTravelCost",
+        "BPIC_2020_RequestForPayment",
+        "BPIC_2020_TravelPermitData",
+    ], discovery_extension="_IMf20")
