f9fa620a5ce46455731bc7405168b6943e7020d5
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2024-05-29 14:09:24 +0300

    Refactor script to measure accuracy in real-life logs

--- tests/icpm24_next_activity_accuracy.py
+++ tests/icpm24_next_activity_accuracy.py
@@ -8,7 +8,7 @@ from pix_framework.io.event_log import read_csv_log, DEFAULT_CSV_IDS
 from process_running_state.reachability_graph import ReachabilityGraph
 
 log_ids = DEFAULT_CSV_IDS
-output_file_path = Path(f"../outputs/synthetic_accuracy.csv")
+output_file_path = Path(f"../outputs/real_life_accuracy.csv")
 
 
 def next_activity_accuracy(datasets: List[str], discovery_extension: str = ""):
@@ -25,25 +25,23 @@ def next_activity_accuracy(datasets: List[str], discovery_extension: str = ""):
         with open(reachability_graph_path, "r") as reachability_graph_file:
             reachability_graph = ReachabilityGraph.from_tgf_format(reachability_graph_file.read())
         # Go over each case ID
-        iasr, ias, occ, mark3, mark5, mark8 = [], [], [], [], [], []
+        ias, mark3, mark4, mark5, mark6 = [], [], [], [], []
         for case_id, data in computed_states.groupby("case_id"):
             # Retrieve remaining activities of this case
             remaining_case = remaining_cases[remaining_cases[log_ids.case] == case_id]
             # Process techniques
-            iasr += [evaluate_state_approximation(data, "IASR", reachability_graph, remaining_case)]
             ias += [evaluate_state_approximation(data, "IAS", reachability_graph, remaining_case)]
-            occ += [evaluate_state_approximation(data, "OCC", reachability_graph, remaining_case)]
-            mark3 += [evaluate_state_approximation(data, "marking-3", reachability_graph, remaining_case)]
-            mark5 += [evaluate_state_approximation(data, "marking-5", reachability_graph, remaining_case)]
-            mark8 += [evaluate_state_approximation(data, "marking-8", reachability_graph, remaining_case)]
+            mark3 += [evaluate_state_approximation(data, "3-gram-index", reachability_graph, remaining_case)]
+            mark4 += [evaluate_state_approximation(data, "4-gram-index", reachability_graph, remaining_case)]
+            mark5 += [evaluate_state_approximation(data, "5-gram-index", reachability_graph, remaining_case)]
+            mark6 += [evaluate_state_approximation(data, "6-gram-index", reachability_graph, remaining_case)]
         # Write stats to file
         with open(output_file_path, "a") as output_file:
-            output_file.write(f"{dataset},{discovery_extension},IASR,{sum(iasr)},{sum(iasr) / len(iasr)}\n")
             output_file.write(f"{dataset},{discovery_extension},IAS,{sum(ias)},{sum(ias) / len(ias)}\n")
-            output_file.write(f"{dataset},{discovery_extension},OCC,{sum(occ)},{sum(occ) / len(occ)}\n")
-            output_file.write(f"{dataset},{discovery_extension},mark-3,{sum(mark3)},{sum(mark3) / len(mark3)}\n")
-            output_file.write(f"{dataset},{discovery_extension},mark-5,{sum(mark5)},{sum(mark5) / len(mark5)}\n")
-            output_file.write(f"{dataset},{discovery_extension},mark-8,{sum(mark8)},{sum(mark8) / len(mark8)}\n")
+            output_file.write(f"{dataset},{discovery_extension},3-gram-index,{sum(mark3)},{sum(mark3) / len(mark3)}\n")
+            output_file.write(f"{dataset},{discovery_extension},4-gram-index,{sum(mark4)},{sum(mark4) / len(mark4)}\n")
+            output_file.write(f"{dataset},{discovery_extension},5-gram-index,{sum(mark5)},{sum(mark5) / len(mark5)}\n")
+            output_file.write(f"{dataset},{discovery_extension},6-gram-index,{sum(mark6)},{sum(mark6) / len(mark6)}\n")
 
 
 def evaluate_state_approximation(
@@ -58,15 +56,19 @@ def evaluate_state_approximation(
     else:
         next_activity = None
     # Retrieve estimated marking
-    estimated_state = data[data["technique"] == technique]["state"].iloc[0]
-    if estimated_state.startswith("Error!"):
-        is_enabled = False
+    results_technique = data[data["technique"] == technique]
+    if len(results_technique) == 1:
+        estimated_state = results_technique["state"].iloc[0]
+        if estimated_state.startswith("Error!"):
+            is_enabled = False
+        else:
+            marking = ast.literal_eval(estimated_state)
+            # Check if the next activity is enabled
+            marking_key = reachability_graph.marking_to_key[tuple(sorted(marking))]
+            outgoing_edges = reachability_graph.outgoing_edges[marking_key]
+            is_enabled = next_activity in [reachability_graph.edge_to_activity[edge] for edge in outgoing_edges]
     else:
-        marking = ast.literal_eval(estimated_state)
-        # Check if the next activity is enabled
-        marking_key = reachability_graph.marking_to_key[tuple(sorted(marking))]
-        outgoing_edges = reachability_graph.outgoing_edges[marking_key]
-        is_enabled = next_activity in [reachability_graph.edge_to_activity[edge] for edge in outgoing_edges]
+        is_enabled = False
     # Return if the next activity is enabled or not
     return is_enabled
 
@@ -81,13 +83,9 @@ if __name__ == '__main__':
     _create_output_file()
     # Process results
     next_activity_accuracy([
-        "Sepsis_Cases",
         "BPIC_2012",
-        "BPIC_2015_1",
-        "BPIC_2015_2",
-        "BPIC_2015_3",
-        "BPIC_2015_4",
-        "BPIC_2015_5",
+        "BPIC_2013_incidents",
+        "BPIC_2014_Activity_log_for_incidents",
         "BPIC_2017",
         "BPIC_2018",
         "BPIC_2019",
@@ -96,4 +94,33 @@ if __name__ == '__main__':
         "BPIC_2020_PrepaidTravelCost",
         "BPIC_2020_RequestForPayment",
         "BPIC_2020_TravelPermitData",
+        "Sepsis_Cases",
+    ], discovery_extension="_IMf50")
+    next_activity_accuracy([
+        "BPIC_2012",
+        "BPIC_2013_incidents",
+        "BPIC_2014_Activity_log_for_incidents",
+        "BPIC_2017",
+        "BPIC_2018",
+        "BPIC_2019",
+        "BPIC_2020_DomesticDeclarations",
+        "BPIC_2020_InternationalDeclarations",
+        "BPIC_2020_PrepaidTravelCost",
+        "BPIC_2020_RequestForPayment",
+        "BPIC_2020_TravelPermitData",
+        "Sepsis_Cases",
     ], discovery_extension="_IMf20")
+    next_activity_accuracy([
+        "BPIC_2012",
+        "BPIC_2013_incidents",
+        "BPIC_2014_Activity_log_for_incidents",
+        "BPIC_2017",
+        "BPIC_2018",
+        "BPIC_2019",
+        "BPIC_2020_DomesticDeclarations",
+        "BPIC_2020_InternationalDeclarations",
+        "BPIC_2020_PrepaidTravelCost",
+        "BPIC_2020_RequestForPayment",
+        "BPIC_2020_TravelPermitData",
+        "Sepsis_Cases",
+    ], discovery_extension="_IMf10")
