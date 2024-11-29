3dd9893a4166345bc15cd48b9f3132e2240282a2
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2024-11-29 22:16:57 +0200

    Refactor evaluation by next activity accuracy (first version)

--- tests/ieeetsc_next_activity_accuracy.py
+++ tests/ieeetsc_next_activity_accuracy.py
@@ -5,50 +5,68 @@ from typing import List
 import pandas as pd
 from pix_framework.io.event_log import read_csv_log, DEFAULT_CSV_IDS
 
-from process_running_state.reachability_graph import ReachabilityGraph
+from process_running_state.petri_net import PetriNet
+from process_running_state.utils import read_petri_net
 
 log_ids = DEFAULT_CSV_IDS
-output_file_path = Path(f"../outputs/real_life_accuracy.csv")
 
 
-def next_activity_accuracy(datasets: List[str], discovery_extension: str = ""):
+def next_activity_accuracy(
+        datasets: List[str],
+        noise_lvl: str = "",
+        discovery_extension: str = ""
+):
     # For each dataset
     for dataset in datasets:
         print(f"\n--- Dataset: {dataset} ---\n")
         # Instantiate paths
-        remaining_cases_path = Path(f"../inputs/real-life/split/{dataset}_remaining.csv.gz")
-        computed_states_path = Path(f"../results/real-life/{dataset}{discovery_extension}_ongoing_states.csv")
-        reachability_graph_path = Path(f"../results/real-life/{dataset}{discovery_extension}_reachability_graph.tgf")
-        # Read preprocessed event log(s)
+        if "synthetic" in dataset:
+            # Synthetic dataset
+            petri_net_path = Path(f"../inputs/synthetic/{dataset}.pnml")
+            remaining_cases_path = Path(f"../inputs/synthetic/split/{dataset}_remaining.csv.gz")
+            output_file_path = Path(f"../outputs/synthetic_accuracy.csv")
+            if noise_lvl == "":
+                computed_states_path = Path(f"../results/synthetic/{dataset}_ongoing_states.csv")
+            else:
+                computed_states_path = Path(f"../results/synthetic/{dataset}_{noise_lvl}_ongoing_states.csv")
+        else:
+            # Real-life log
+            petri_net_path = Path(f"../inputs/real-life/{dataset}{discovery_extension}.pnml")
+            remaining_cases_path = Path(f"../inputs/real-life/split/{dataset}_remaining.csv.gz")
+            computed_states_path = Path(f"../results/real-life/{dataset}{discovery_extension}_ongoing_states.csv")
+            output_file_path = Path(f"../outputs/real_life_accuracy.csv")
+        # Create results file if it doesn't exist
+        if not output_file_path.exists():
+            with open(output_file_path, "w") as output_file:
+                output_file.write("dataset,discovery_algorithm,technique,abs_positives,rel_positives\n")
+        # Read preprocessed event log(s) and Petri net
         remaining_cases = read_csv_log(remaining_cases_path, log_ids, sort=True)
         computed_states = pd.read_csv(computed_states_path)
-        with open(reachability_graph_path, "r") as reachability_graph_file:
-            reachability_graph = ReachabilityGraph.from_tgf_format(reachability_graph_file.read())
+        petri_net = read_petri_net(petri_net_path)
         # Go over each case ID
-        ias, mark3, mark4, mark5, mark6 = [], [], [], [], []
+        evaluated_techniques = computed_states["technique"].unique()
+        results = {technique: [] for technique in evaluated_techniques}
         for case_id, data in computed_states.groupby("case_id"):
             # Retrieve remaining activities of this case
             remaining_case = remaining_cases[remaining_cases[log_ids.case] == case_id]
             # Process techniques
-            ias += [evaluate_state_approximation(data, "IAS", reachability_graph, remaining_case)]
-            mark3 += [evaluate_state_approximation(data, "3-gram-index", reachability_graph, remaining_case)]
-            mark4 += [evaluate_state_approximation(data, "4-gram-index", reachability_graph, remaining_case)]
-            mark5 += [evaluate_state_approximation(data, "5-gram-index", reachability_graph, remaining_case)]
-            mark6 += [evaluate_state_approximation(data, "6-gram-index", reachability_graph, remaining_case)]
+            for technique in evaluated_techniques:
+                results[technique] += [
+                    evaluate_state_approximation(data, results[technique], remaining_case, petri_net)
+                ]
         # Write stats to file
         with open(output_file_path, "a") as output_file:
-            output_file.write(f"{dataset},{discovery_extension},IAS,{sum(ias)},{sum(ias) / len(ias)}\n")
-            output_file.write(f"{dataset},{discovery_extension},3-gram-index,{sum(mark3)},{sum(mark3) / len(mark3)}\n")
-            output_file.write(f"{dataset},{discovery_extension},4-gram-index,{sum(mark4)},{sum(mark4) / len(mark4)}\n")
-            output_file.write(f"{dataset},{discovery_extension},5-gram-index,{sum(mark5)},{sum(mark5) / len(mark5)}\n")
-            output_file.write(f"{dataset},{discovery_extension},6-gram-index,{sum(mark6)},{sum(mark6) / len(mark6)}\n")
+            for technique in evaluated_techniques:
+                total_accuracy = sum(results[technique])
+                partial_accuracy = sum(results[technique]) / len(results[technique])
+                output_file.write(f"{dataset},{discovery_extension},{technique},{total_accuracy},{partial_accuracy}\n")
 
 
 def evaluate_state_approximation(
         data: pd.DataFrame,
         technique: str,
-        reachability_graph: ReachabilityGraph,
-        remaining_case: pd.DataFrame
+        remaining_case: pd.DataFrame,
+        petri_net: PetriNet
 ) -> bool:
     # Retrieve next activity
     if len(remaining_case) > 0:
@@ -64,23 +82,15 @@ def evaluate_state_approximation(
         else:
             marking = ast.literal_eval(estimated_state)
             # Check if the next activity is enabled
-            marking_key = reachability_graph.marking_to_key[tuple(sorted(marking))]
-            outgoing_edges = reachability_graph.outgoing_edges[marking_key]
-            is_enabled = next_activity in [reachability_graph.edge_to_activity[edge] for edge in outgoing_edges]
+            enabled_activities = [enabled_activity for enabled_activity, _ in petri_net.advance_full_marking(marking)]
+            is_enabled = next_activity in enabled_activities
     else:
         is_enabled = False
     # Return if the next activity is enabled or not
     return is_enabled
 
 
-def _create_output_file():
-    with open(output_file_path, "w") as output_file:
-        output_file.write("dataset,discovery_algorithm,technique,abs_positives,rel_positives\n")
-
-
 if __name__ == '__main__':
-    # Create output metrics path
-    _create_output_file()
     # Process results
     next_activity_accuracy([
         "BPIC_2012",
@@ -124,3 +134,39 @@ if __name__ == '__main__':
         "BPIC_2020_TravelPermitData",
         "Sepsis_Cases",
     ], discovery_extension="_IMf10")
+    next_activity_accuracy([
+        "synthetic_and_k3",
+        "synthetic_and_k5",
+        "synthetic_and_k10",
+        "synthetic_and_kinf",
+        "synthetic_xor_sequence",
+        "synthetic_xor_loop",
+        "synthetic_nondeterministic",
+    ])
+    next_activity_accuracy([
+        "synthetic_and_k3",
+        "synthetic_and_k5",
+        "synthetic_and_k10",
+        "synthetic_and_kinf",
+        "synthetic_xor_sequence",
+        "synthetic_xor_loop",
+        "synthetic_nondeterministic",
+    ], noise_lvl="noise_1")
+    next_activity_accuracy([
+        "synthetic_and_k3",
+        "synthetic_and_k5",
+        "synthetic_and_k10",
+        "synthetic_and_kinf",
+        "synthetic_xor_sequence",
+        "synthetic_xor_loop",
+        "synthetic_nondeterministic",
+    ], noise_lvl="noise_2")
+    next_activity_accuracy([
+        "synthetic_and_k3",
+        "synthetic_and_k5",
+        "synthetic_and_k10",
+        "synthetic_and_kinf",
+        "synthetic_xor_sequence",
+        "synthetic_xor_loop",
+        "synthetic_nondeterministic",
+    ], noise_lvl="noise_3")
