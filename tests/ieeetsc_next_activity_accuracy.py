9ec8e4e7a28641cbc8e36b3d58c59670e3ce0059
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2024-12-08 15:28:17 +0200

    [IEEE Next Activity Accuracy] refactor to speed up computation

--- tests/ieeetsc_next_activity_accuracy.py
+++ tests/ieeetsc_next_activity_accuracy.py
@@ -6,6 +6,7 @@ import pandas as pd
 from pix_framework.io.event_log import read_csv_log, DEFAULT_CSV_IDS
 
 from process_running_state.petri_net import PetriNet
+from process_running_state.reachability_graph import ReachabilityGraph
 from process_running_state.utils import read_petri_net
 
 log_ids = DEFAULT_CSV_IDS
@@ -32,27 +33,33 @@ def next_activity_accuracy(
             output_file_path = Path(f"../outputs/synthetic_accuracy.csv")
             if noise_lvl == "":
                 computed_states_path = Path(f"../results/synthetic/{dataset}_ongoing_states.csv")
+                reach_graph_path = Path(f"../results/synthetic/{dataset}_reachability_graph.tgf")
             else:
                 computed_states_path = Path(f"../results/synthetic/{dataset}_{noise_lvl}_ongoing_states.csv")
+                reach_graph_path = Path(f"../results/synthetic/{dataset}_{noise_lvl}_reachability_graph.tgf")
         else:
             # Real-life log
             petri_net_path = Path(f"../inputs/real-life/{dataset}{discovery_extension}.pnml")
             remaining_cases_path = Path(f"../inputs/real-life/split/{dataset}_remaining.csv.gz")
             computed_states_path = Path(f"../results/real-life/{dataset}{discovery_extension}_ongoing_states.csv")
+            reach_graph_path = Path(f"../results/real-life/{dataset}{discovery_extension}_reachability_graph.tgf")
             output_file_path = Path(f"../outputs/real_life_accuracy.csv")
         # Create results file if it doesn't exist
         if not output_file_path.exists():
             with open(output_file_path, "w") as output_file:
                 output_file.write("dataset,discovery_algorithm,noise_lvl,technique,"
                                   "num_positives,num_reachable_positives,num_unreachable\n")
-        # Read preprocessed event log(s) and Petri net
+        # Read preprocessed event log(s), Petri net, and reachability graph
         remaining_cases = read_csv_log(remaining_cases_path, log_ids, sort=True)
         computed_states = pd.read_csv(computed_states_path)
         num_cases = len(computed_states["case_id"].unique())
         petri_net = read_petri_net(petri_net_path)
+        with open(reach_graph_path, 'r') as reachability_graph_file:
+            reachability_graph = ReachabilityGraph.from_tgf_format(reachability_graph_file.read())
         # Compute reachable markings and initialize results
         print("Computing all reachable markings!")
         reachable_markings = petri_net.compute_reachable_markings()
+        petri_net.repair_mixed_decision_points()  # Repair for the "advance_full_marking" operation to work properly
         evaluated_techniques = set(computed_states["technique"].unique()) & all_techniques
         results = {technique: [] for technique in evaluated_techniques}
         # Go over each case ID
@@ -64,7 +71,14 @@ def next_activity_accuracy(
             # Process techniques
             for technique in evaluated_techniques:
                 results[technique] += [
-                    evaluate_state_approximation(data, technique, remaining_case, petri_net, reachable_markings)
+                    evaluate_state_approximation(
+                        data,
+                        technique,
+                        remaining_case,
+                        petri_net,
+                        reachability_graph,
+                        reachable_markings
+                    )
                 ]
             # Report progress
             if i % 500 == 0:
@@ -87,6 +101,7 @@ def evaluate_state_approximation(
         technique: str,
         remaining_case: pd.DataFrame,
         petri_net: PetriNet,
+        reachability_graph: ReachabilityGraph,
         reachable_markings: Set[Tuple[str]],
 ) -> Tuple[bool, bool]:
     # Retrieve next activity
@@ -110,11 +125,21 @@ def evaluate_state_approximation(
                 # End of trace, check marking is final
                 is_enabled = petri_net.is_final_marking(marking)
             else:
-                # Check if the next activity is enabled
-                enabled_activities = [
-                    petri_net.id_to_transition[enabled_activity].name
-                    for enabled_activity, _ in petri_net.advance_full_marking(marking)
-                ]
+                # Retrieve enabled activities
+                if marking_key in reachability_graph.marking_to_key:
+                    # Marking exists in reachability graph, retrieve outgoing edges (labels)
+                    marking_id = reachability_graph.marking_to_key[marking_key]
+                    enabled_activities = [
+                        reachability_graph.edge_to_activity[edge_id]
+                        for edge_id in reachability_graph.outgoing_edges[marking_id]
+                    ]
+                else:
+                    # Compute "advance full marking" in petri net to reach all enabled activities from this marking
+                    enabled_activities = [
+                        petri_net.id_to_transition[enabled_activity].name
+                        for enabled_activity, _ in petri_net.advance_full_marking(marking)
+                    ]
+                # Check if next activity is enabled
                 is_enabled = next_activity in enabled_activities
     else:
         is_enabled = False
